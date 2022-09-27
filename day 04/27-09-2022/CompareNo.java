3c7ccfbd2da948ee1ed02977ea5247010eb08476
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-09-27 21:12:37 +0530

    Update CompareNo.java

--- day 04/27-09-2022/CompareNo.java
+++ day 04/27-09-2022/CompareNo.java
@@ -2,52 +2,125 @@
 user than accordingly accept the no from the user and find 
 out the greater no,smaller no, ascending, Descending  */
 
-import java.util.*;
-public class CompareNo
-{
-public static void main(string args[])
-{
-  accept_value()
-    
-}
-  public satic void accept_value()
-  {
-    //taking input from user
-  Scanner sc = new Scanner(System.in);
-  int n; //declaring variable
-   //Ask the user to enter array size
-    System.out.println("Enter a size of number set:");
-   n=sc.nextInt();
-    
-     //Declare the array
-     int arr[] = new int[n]; 
-     System.out.println("Enter numbers in set:");
-     for(int i=0;i<n;i++)   //Initialize the array
-       {
-            arr[i]=sc.nextInt();
-        }
+import java.util.Scanner;
+public class CompareNo {
 
-      }
-  
-  public static void greaterno(int[]a,int total)
-  {
-  int temp;  
-for (int i = 0; i < total; i++)   
-        {  
-            for (int j = i + 1; j < total; j++)   
-            {  
-                if (a[i] > a[j])   
-                {  
-                    temp = a[i];  
-                    a[i] = a[j];  
-                    a[j] = temp;  
-                }  
-            }  
-        }  
-       return a[total-1];  
-  }
-  public static void smallerNo(){}
-  public static void ascendingNo(){}
-  public static void DescendingNo(){}
-  
-  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ch;
+		arraySortFromUser obj = new arraySortFromUser();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("1. Find Greater\n"
+				+ "2. Find Smaller\n"
+				+ "3. Ascending\n"
+				+ "4. Descending\n");
+		ch=sc.nextInt();
+		switch (ch) {
+		case 1:
+			obj.greater();
+			break;
+		case 2:
+			obj.smaller();
+			break;
+		case 3:
+			obj.asc();
+			break;
+		case 4:
+			obj.desc();
+			break;
+
+		default:
+			break;
+		}
+	}
+	public int[] accept() {
+		Scanner sc = new Scanner(System.in);
+		int size,i;
+		System.out.println("Enter size");
+		size=sc.nextInt();
+		int no[]=new int[size];
+		System.out.println("Enter "+size+" numbers:");
+		for(i=0;i<size;i++) {
+			no[i]=sc.nextInt();
+		}
+		return no;
+	}
+	public void greater() {
+		int i;
+		int[] arr = accept();
+		int max=arr[0];
+		for (i = 1; i < arr.length; i++)
+            if (arr[i] > max)
+                max = arr[i];
+		System.out.println("Greatest number in the given array is " +max);
+	}
+	public void smaller() {
+		int i;
+		int[] arr=accept();
+		int min=arr[0];
+		for (i = 0; i < arr.length; i++)
+            if (arr[i] < min)
+                min = arr[i];
+		System.out.println("Minumum number in the given array is " +min);
+				
+	}
+	public void asc() {
+		
+		 int temp = 0;    
+		 int[] arr=accept();
+	     //Displaying elements of original array    
+	     System.out.println("Elements of original array: ");    
+	     for (int i = 0; i < arr.length; i++) {     
+	         System.out.print(arr[i] + " ");    
+	     }    
+	            
+	     //Sort the array in ascending order    
+	     for (int i = 0; i < arr.length; i++) {     
+	         for (int j = i+1; j < arr.length; j++) {     
+	            if(arr[i] > arr[j]) {    
+	                temp = arr[i];    
+	                arr[i] = arr[j];    
+	                arr[j] = temp;    
+	            }     
+	         }     
+	     }    
+	          
+	     System.out.println();    
+	            
+	     //Displaying elements of array after sorting    
+	     System.out.println("Elements of array sorted in ascending order: ");    
+	     for (int i = 0; i < arr.length; i++) {     
+	         System.out.print(arr[i] + " ");    
+	     }
+	}
+	public void desc() {
+		
+		 int temp = 0;    
+		 int[] arr=accept();
+	     //Displaying elements of original array    
+	     System.out.println("Elements of original array: ");    
+	     for (int i = 0; i < arr.length; i++) {     
+	         System.out.print(arr[i] + " ");    
+	     }    
+	            
+	     //Sort the array in ascending order    
+	     for (int i = 0; i < arr.length; i++) {     
+	         for (int j = i+1; j < arr.length; j++) {     
+	            if(arr[i] < arr[j]) {    
+	                temp = arr[i];    
+	                arr[i] = arr[j];    
+	                arr[j] = temp;    
+	            }     
+	         }     
+	     }    
+	          
+	     System.out.println();    
+	            
+	     //Displaying elements of array after sorting    
+	     System.out.println("Elements of array sorted in descending order: ");    
+	     for (int i = 0; i < arr.length; i++) {     
+	         System.out.print(arr[i] + " ");    
+	     }
+	}
+
+}
