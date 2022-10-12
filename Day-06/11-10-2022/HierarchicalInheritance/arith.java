36bcb0d92cd9d1f58bf018953f65832514666054
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-12 19:26:57 +0530

    Create arith.java

---
+++ Day-06/11-10-2022/HierarchicalInheritance/arith.java
@@ -0,0 +1,18 @@
+public class arith extends accept {
+	// public static void main(String[] args) {
+	// 	arith obj = new arith();
+	// 	obj.arithmetic();
+	// }
+	public void arithmetic() {
+		acpt();
+		//Add
+		System.out.println("Addition of Numbers is "+(num1+num2));
+		//Multi
+		System.out.println("Multiplication of Numbers is "+(num1*num2));
+		//Div
+		System.out.println("Division of Numbers is "+(num1/num2));
+		//Remainder
+		System.out.println("Remainder of is "+(num1%num2));
+	}
+
+}
