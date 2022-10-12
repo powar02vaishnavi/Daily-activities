5ec06b5092eaf2838421f2a73cfb71f2ad27624a
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-12 19:36:03 +0530

    Create run.java

---
+++ Day-06/11-10-2022/multiLevelInheritance/run.java
@@ -0,0 +1,11 @@
+public class run extends marksAvg{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//calling functions inherited by multilevel inheritance
+		pr();
+		swp();
+		marks();
+	}
+
+}
