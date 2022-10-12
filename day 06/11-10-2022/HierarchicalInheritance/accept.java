24e597e47a175261f8568fdd04c02340acf6333b
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-12 19:56:07 +0530

    Create accept.java

---
+++ day 06/11-10-2022/HierarchicalInheritance/accept.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class accept {
+	int num1,num2;
+	public void acpt() {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		//taking user input
+		System.out.println("Enter Two Values");
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+	}
+
+}
