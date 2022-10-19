7896da9242b33f0e493956cb27a3a96fc42e957a
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-19 18:54:58 +0530

    Create protected1.java

---
+++ day 08/18-10-2022/modifiers/protected1.java
@@ -0,0 +1,11 @@
+package modifiers;
+
+public class protected1 extends protected {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		prot1 obj = new prot1(); 
+	    obj.display();
+	}
+
+}
