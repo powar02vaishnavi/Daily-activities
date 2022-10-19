1524da2480c76b983e082d8697f301a91c78801e
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-19 18:50:23 +0530

    Create protec.java

---
+++ day 08/18-10-2022/modif/protec.java
@@ -0,0 +1,12 @@
+package modif;
+import modifiers.*;
+
+public class protec extends prot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		protec obj = new protec(); 
+	    obj.display();
+	}
+
+}
