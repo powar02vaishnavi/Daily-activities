6b548fe957a21f2a05e5ecc788a1f0612ef4f09d
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-19 18:56:13 +0530

    Create public1.java

---
+++ day 08/18-10-2022/modifiers/public1.java
@@ -0,0 +1,11 @@
+package modifiers;
+
+public class public1 extends public {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		pub obj=new pub();
+		obj.process();
+	}
+
+}
