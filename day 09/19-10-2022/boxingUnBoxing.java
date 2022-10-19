cbe35632edfc6ae1dba24e4d5cd429c2209198cc
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-19 19:54:23 +0530

    Create boxingUnBoxing.java

---
+++ day 09/19-10-2022/boxingUnBoxing.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class boxingUnBoxing {
+	static Scanner sc=new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Boxing();
+		Unboxing();
+	}
+	//+ or - using box
+	public static void Boxing() {
+		System.out.println("This is Box Method :\n");
+		//taking input as int datatype
+		System.out.println("Enter a number");
+		int num=sc.nextInt();
+		//assigning int num in obj named it1
+		Integer it=new Integer(num);
+		//check if number is positive or negative
+		if(it>0) {
+			System.out.println(+it+" is Positive Value");
+		}
+		else if(it==0) {
+			System.out.println(+it+" is Neither Positive Nor Negative Value");
+		}
+		else {
+			System.out.println(+it+" is Negative Value");
+		}
+	}
+	//div by 3 or not using unbox
+	public static void Unboxing() {
+		System.out.println("\nThis is UnBox Method :\n");
+		System.out.println("Enter the value");
+		//assigning value to obj
+		Integer obj=sc.nextInt();
+		//assigning obj value in num variable
+		int num=obj;
+		//checking if number is divisible by 3 or not
+		if(num%3==0) {
+			System.out.println(+num+" is Divisible by 3");
+		}
+		else {
+			System.out.println(+num+" is Not Divisible by 3");
+		}
+		
+	}
+
+}
