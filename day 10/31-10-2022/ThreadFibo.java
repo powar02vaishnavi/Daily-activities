c522753aa65efd43a16d0378d4620ded4ab17de0
Vaishnavi Powar <59617469+powar02vaishnavi@users.noreply.github.com>
2022-10-31 19:21:20 +0530

    Update ThreadFibo.java

--- day 10/31-10-2022/ThreadFibo.java
+++ day 10/31-10-2022/ThreadFibo.java
@@ -1,1 +1,26 @@
 /* Thread Class: Display Fibonacci series */
+
+package thread1;
+public class ThreadFibo extends Thread{
+	public static void main(String[] args) {
+	ThreadFibo ob=new ThreadFibo();
+	ob.start();
+	ThreadFibo ob1=new ThreadFibo();
+	ob1.start();
+	}
+	public void run()
+{
+  int n, a = 0, b = 0, c = 1;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter value of n:");
+        n = s.nextInt();
+        System.out.print("Fibonacci Series:");
+        for(int i = 1; i <= n; i++)
+        {
+            a = b;
+            b = c;
+            c = a + b;
+            System.out.print(a+" ");
+        }
+}
+}
